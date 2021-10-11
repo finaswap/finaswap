e1f0f6750ada5aad0b37466dc4cde25d96bcdbda
finaswap <github@finaswap.com>
2021-10-11 11:02:25 +0200

    Inherit ownable

--- contracts/governance/FinaToken.sol
+++ contracts/governance/FinaToken.sol
@@ -5,7 +5,7 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FinaToken {
+contract FinaToken is Ownable {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
