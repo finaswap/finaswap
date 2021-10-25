a849fbcf0050aef0a34e84b7d9c38e9b73fe5b66
finaswap <github@finaswap.com>
2021-10-25 15:59:15 +0200

    Update deployment

--- deploy/FinaJuniorMasterV2.js
+++ deploy/FinaJuniorMasterV2.js
@@ -1,30 +1,17 @@
 const { ChainId } = require("@finaswap/sdk")
 
-
-const FNA = {
-	[ChainId.ROPSTEN]: '0x4dB99c31039a17F627f26CC831b8DCebbd958CfC'
-}
-
 module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
   const { deploy } = deployments
 
   const { deployer, dev } = await getNamedAccounts()
 
   const chainId = await getChainId()
-
-  let finaAddress;
-
-  if (chainId === '31337') {
-    finaAddress = (await deployments.get("FinaToken")).address
-  } else if (chainId in FNA) {
-    finaAddress = FNA[chainId]
-  } else {
-    throw Error("No FNA!")
-  }
-
+  
+  const fina = await ethers.getContract("FinaToken")
+  
   await deploy("FinaJuniorMasterV2", {
     from: deployer,
-    args: [finaAddress],
+    args: [fina.address],
     log: true,
     deterministicDeployment: false
   })
--- deploy/FinaToken.js
+++ deploy/FinaToken.js
@@ -9,9 +9,15 @@
     deterministicDeployment: false
   })
   
-  const finaToken = await ethers.getContract("FinaToken")
-  const finaTokenOwner = await finaToken.owner()
-  await finaToken.mint(finaTokenOwner, "200000000000000000000000000")
+  const chainId = await getChainId()
+  
+  //Only mint on BSC mainnet, Ropsten, Goerli, Kovan, and Rinkeby
+  if (chainId == "56" || chainId == "3" || chainId == "4" || chainId == "5" || chainId == "42")
+  {
+	const finaToken = await ethers.getContract("FinaToken")
+	const finaTokenOwner = await finaToken.owner()
+	await finaToken.mint(finaTokenOwner, "200000000000000000000000000")
+  }
 }
 
 module.exports.tags = ["FinaToken"]
