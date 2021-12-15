81c4a55f6243768e1afd3c0bbb39a4bb3c9242c1
finaswap <github@finaswap.com>
2021-12-15 15:48:07 +0100

    Replace FinaToken

--- deploy/FinaToken.js
+++ deploy/FinaToken.js
@@ -1,14 +1,32 @@
- module.exports = async function ({ getNamedAccounts, deployments }) {
-  const { deploy } = deployments
+const {
+  bytecode,
+  abi,
+} = require("../deployments/finatoken/FinaToken.json");
 
-  const { deployer } = await getNamedAccounts()
+module.exports = async function ({
+  ethers,
+  getNamedAccounts,
+  deployments,
+  getChainId,
+}) {
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+  
+  const name = "FinaToken";
+  const symbol = "FNA";
 
   await deploy("FinaToken", {
+    contract: {
+      abi,
+      bytecode,
+    },
     from: deployer,
+    args: [name, symbol],
     log: true,
-    deterministicDeployment: false
-  })  
-}
+    deterministicDeployment: false,
+  });
+};
 
 module.exports.tags = ["FinaToken"]
 module.exports.dependencies = ["UniswapV2Factory", "UniswapV2Router02"]
